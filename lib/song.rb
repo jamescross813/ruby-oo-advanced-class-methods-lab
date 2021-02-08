55daf305fe72365061545fe5e7914fd8bc9a75e4
jamescross <crossjames813@gmail.com>
2021-02-08 17:24:12 -0600

    commit tree 30183122871c13b77fd2a3a6d3f093ab74e7729a

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,80 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
-  def self.all
+  @@titles = []
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    
+    song
+  end
+
+  def self.create_by_name(title)
+    song = self.create
+    song.name = title
+    song
+  end
+
+  def self.find_by_name(title)
+    self.all.find{|song| song.name == title}
+  end
+
+  def self.find_or_create_by_name(title)
+    if self.find_by_name(title) #check to see if it already exists
+      self.find_by_name(title) 
+    else 
+      self.create_by_name(title) #creates new one if it doesn't exist
+   end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|a| [a.name]}
+  end
+
+  def self.new_from_filename(info)
+    
+    data = info.split(" - ")
+    artist_name = data[0]
+    title_raw = data [1]
+    title = title_raw.split(".")
+    song = self.new
+    song.name = title[0]
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(info)
+    data = info.split(" - ")
+    artist_name = data[0]
+    title_raw = data [1]
+    title = title_raw.split(".")
+    song = self.create
+    song.name = title[0]
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.all 
     @@all
   end
 
   def save
     self.class.all << self
   end
-
+  
 end
+
